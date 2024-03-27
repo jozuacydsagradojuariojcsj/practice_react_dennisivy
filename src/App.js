d04dac9b661dcd487d459b5cb531adf02c4434b3
jozuacydsagradojuariojcsj <jozuacydjuarioJSCJ@gmail.com>
2024-03-27 21:24:33 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import { Box,Grid,TextField,Button} from '@mui/material'
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [passwordStrength, setPasswordStrength] = useState("")
+
+  const emailText = (e)=>{
+    setEmail(e.target.value);
+  }
+
+  const passwordStrengthCheck = (password)=>{
+    if (password.length >= 8 && /[!@#$%^&*]/.test(password)){
+      setPasswordStrength('Strong')
+    }else if (password.length>=6){
+      setPasswordStrength('Medium')
+    }else{
+      setPasswordStrength('Weak')
+    }
+  }
+
+  const passwordText =(e)=>{
+    const passwordNew = e.target.value;
+    setPassword(passwordNew);
+    passwordStrengthCheck(passwordNew);
+  }
+
+  const submitData = (e)=>{
+    e.preventDefault()
+
+    console.log('Email:',email);
+    console.log('Password:',password)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid container spacing={0} direction='column' justifyContent='center' alignItems='center' sx={{minHeight:'100vh'}}>
+        <Box sx={{
+          height:400,
+          width:500,
+          border:'2px solid black',
+          borderRadius:'30px',
+          justifyContent:'center',
+          alignItems:'center',
+          display:'flex'
+        }}>
+          <Grid container spacing={0} direction='column' justifyContent='center' alignItems='center'>
+            <Grid item sx={{paddingBottom:2}}>
+              <TextField required onChange={emailText} value={email} type='text' id="outlined-basic" label="Username" variant="outlined"  InputProps={{sx:{borderRadius:'50px', '& fieldset':{borderColor:'black'}}}} />
+            </Grid>
+            <Grid>
+              <TextField required onChange={passwordText} value={password} type='password' id="outlined-basic" label="Password" variant="outlined" InputProps={{sx:{borderRadius:'50px', '& fieldset':{borderColor:'black'}}}} />
+            </Grid>
+            <Grid>
+              Password Strength : {passwordStrength}
+            </Grid>
+            <Grid>
+            <Button variant="contained" onClick={submitData}>submit</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+    </>
   );
 }
 
